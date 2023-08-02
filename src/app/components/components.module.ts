9077f52d633798e84d9fef06f0dc6d0898a883f7
Gustavo Kropiwiec <gustavosk17@hotmail.com>
2023-08-02 20:22:30 -0300

    headline and header components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,14 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { HeadlineComponent } from './headline/headline.component';
+import { ButtonComponent } from './button/button.component';
+import { AboutComponent } from './about/about.component';
 
 const moduleComponents = [
     HeaderComponent,
-    HeadlineComponent
+    HeadlineComponent,
+    ButtonComponent,
+    AboutComponent
 ];
 
 @NgModule({
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+    private navHeader!: HTMLElement | null;
 
+    @HostListener('window:scroll', ['$event'])
+    onScroll() {
+        if (window.scrollY <= 100 && window.innerWidth >= 992) {
+            this.navHeader?.classList.remove('nav-bg-scroll');
+        } else {
+            this.navHeader?.classList.add('nav-bg-scroll');
+        }
+    }
+
+    ngAfterViewInit(): void {
+        this.navHeader = document.getElementById('nav-header');
+        if (window.innerWidth >= 992) 
+            this.navHeader?.classList.remove('nav-bg-scroll');
+    }
 }
